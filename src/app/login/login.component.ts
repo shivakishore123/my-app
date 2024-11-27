15889ea93f26ffbff124a88e5f1edd3b57f92fc8
shivakishore123 <shivakishore777@gmail.com>
2024-11-27 10:27:59 +0530

    login form completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl , FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  public loginForm:FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  })
+  login(){
+    console.log(this.loginForm);
+  }
 }
